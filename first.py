7b5cbdeb875bb136b73f1ae351c50bfad2e31a03
unknown <kushalthapa1373@gmail.com>
2022-08-07 12:13:40 +0545

    sum of programme

--- first.py
+++ first.py
@@ -1,1 +1,13 @@
 print("kushal thapa")
+
+print(8+2)
+
+''''code for gitbash
+git status(to look changes status of file)
+git add . 
+git commit -m "any message" (to give any messages)
+git remote add origin (paste the reprository link)
+
+git push origin master 
+
+'''
